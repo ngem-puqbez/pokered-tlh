8855022e1d7d483b915195df896fc2aabf3f868a
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-21 01:50:32 -0500

    ENG to TLH

--- text/Route2.asm
+++ text/Route2.asm
@@ -1,9 +1,9 @@
 _Route2SignText::
-	text "ROUTE 2"
-	line "VIRIDIAN CITY -"
-	cont "PEWTER CITY"
+	text "He cha'"
+	line "<q><o><'><r><I><n> <v><e><n><g> -"
+	cont "<H><o><t><n><a><g><h> <v><e><n><g>"
 	done
 
 _Route2DiglettsCaveSignText::
-	text "DIGLETT's CAVE"
+	text "<g><h><a><w><g><h><u> <D><I><S>"
 	done
