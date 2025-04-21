49c601da725be90037b267200001d7010fbfea75
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-20 21:11:48 -0500

    switch ENG to TLH
    all strings updated to reflect Klingon language translation/Localization

--- text/WardensHouse.asm
+++ text/WardensHouse.asm
@@ -1,86 +1,84 @@
 _WardensHouseWardenGibberish1Text::
-	text "WARDEN: Hif fuff"
-	line "hefifoo!"
+	text "<w><a><m><w><I><'><'><a><'>: 'e 'u"
+	line "'e'I'uv...!"
 
-	para "Ha lof ha feef ee"
-	line "hafahi ho. Heff"
-	cont "hee fwee!"
+	para "'a HowIj 'I 'ej ve"
+	line "'avaHIy 'o. Hev"
+	cont "ve 'ewe!"
 	done
 
 _WardensHouseWardenGibberish2Text::
-	text "Ah howhee ho hoo!"
-	line "Eef ee hafahi ho!"
+	text "'a jowHe Ho ju!"
+	line "Ho 'o HavaHI Ho!"
 	done
 
 _WardensHouseWardenGibberish3Text::
-	text "Ha? He ohay heh"
-	line "ha hoo ee haheh!"
+	text "Hu? He 'oyay He"
+	line "va 'u 'e Hghgh!"
 	done
 
 _WardensHouseWardenGaveTheGoldTeethText::
-	text "<PLAYER> gave the"
-	line "GOLD TEETH to the"
-	cont "WARDEN!@"
+	text "<w><a><m><w><I><'><'><a><'>vaD"
+	line "<H><o><'><D><u><'> <n><g><e><b>"
+	cont "nob <PLAYER>!@"
 	text_end
 
 _WardensHouseWardenTeethPoppedInHisTeethText::
 	text_start
 
-	para "The WARDEN popped"
-	line "in his teeth!"
+	para "Ho'Du'Daj lelHa'"
+	line "<w><a><m><w><I><'><a><'>!"
 	prompt
 
 _WardensHouseWardenThanksText::
-	text "WARDEN: Thanks,"
-	line "kid! No one could"
-	cont "understand a word"
-	cont "that I said."
+	text "<w><a><m><w><I><'><a><'>: majQa'"
+	line "puq! jIjatlh 'ach"
+	cont "mu'meywIj yajlaH"
+	cont "pagh."
 
-	para "I couldn't work"
-	line "that way."
-	cont "Let me give you"
-	cont "something for"
-	cont "your trouble."
+	para "Do'Ha' ghu'vetlh"
+	line "vIDIghtaHvIS"
+	cont "jIvumlaHbe'ba'!"
+	cont "qathlo'meH popvam"
+	cont "qanob."
 	prompt
 
 _WardensHouseWardenReceivedHM04Text::
-	text "<PLAYER> received"
-	line "@"
-	text_ram wStringBuffer
-	text "!@"
+	text "@"
+	text_ram wcf4b
+	text_start
+	line "Suq <PLAYER>!@"
 	text_end
 
 _WardensHouseWardenHM04ExplanationText::
-	text "WARDEN: HM04"
-	line "teaches STRENGTH!"
+	text "<w><a><m><w><I><'><'><a><'>: <y><u><v>"
+	line "Ho'DoS ngaS <H><j>04!"
 
-	para "It lets #MON"
-	line "move boulders"
-	cont "when you're out-"
-	cont "side of battle."
+	para "Suvbe'taHvIS"
+	line "naghmey tIn vIHmoH"
+	cont "Ho'DoSvam."
 
-	para "Oh yes, did you"
-	line "find SECRET HOUSE"
-	cont "in SAFARI ZONE?"
+	para "toH, <c><h><o><n> <y><o><t><l><h>Daq"
+	line "qach pegh"
+	cont "DaSamta''a'?"
 
-	para "If you do, you"
-	line "win an HM!"
+	para "pa' <H><j> DaSuqlaH"
+	line "je!"
 
-	para "I hear it's the"
-	line "rare SURF HM."
+	para "<Q><a><l> Ho'DoS ghojmoH"
+	line "<H><j>vetlh qub."
 	done
 
 _WardensHouseWardenHM04NoRoomText::
-	text "Your pack is"
-	line "stuffed full!"
+	text "wejHa' buy'chu'"
+	line "buq DaqengtaHbogh!"
 	done
 
 _WardensHouseDisplayPhotosAndFossilsText::
-	text "#MON photos"
-	line "and fossils."
+	text "#<m><a><n> mIlloghmey"
+	line "'an'ormey je."
 	done
 
 _WardensHouseDisplayMerchandiseText::
-	text "Old #MON"
-	line "merchandise."
+	text "#<m><a><n> 'aH ngo'."
 	done
