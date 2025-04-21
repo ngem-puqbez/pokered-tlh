57e37ae5a24f5e819242a3ad4302f93b5aec94b7
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-21 16:56:40 -0500

    ENG to TLH

--- engine/menus/players_pc.asm
+++ engine/menus/players_pc.asm
@@ -241,10 +241,10 @@ PlayerPCToss:
 	jp .loop
 
 PlayersPCMenuEntries:
-	db   "WITHDRAW ITEM"
-	next "DEPOSIT ITEM"
-	next "TOSS ITEM"
-	next "LOG OFF@"
+	db   "luch Suqqa'"
+	next "luch pol"
+	next "luch woD"
+	next "ngaQmoH@"
 
 TurnedOnPC2Text:
 	text_far _TurnedOnPC2Text
