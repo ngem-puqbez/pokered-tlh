329fd14021d9a2355301eb2a06acaf3b5f5dff58
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-20 21:27:44 -0500

    ENG to TLH

--- text/ViridianForestSouthGate.asm
+++ text/ViridianForestSouthGate.asm
@@ -1,13 +1,13 @@
 _ViridianForestSouthGateGirlText::
-	text "Are you going to"
-	line "VIRIDIAN FOREST?"
-	cont "Be careful, it's"
-	cont "a natural maze!"
+	text "<q><o><'><r><I><n> <n><g><e><m>Daq"
+	line "Daleng DaneH'a'?"
+	cont "yIyep! chen'ong"
+	cont "but 'oH ngem'e'!"
 	done
 
 _ViridianForestSouthGateLittleGirlText::
-	text "RATTATA may be"
-	line "small, but its"
-	cont "bite is wicked!"
-	cont "Did you get one?"
+	text "mach <r><I><S><'><a><b>"
+	line "'ach chopchugh"
+	cont "rIQqu'moHlaH!"
+	cont "Dajonta''a'?"
 	done
