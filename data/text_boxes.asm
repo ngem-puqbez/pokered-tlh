754e24ada6e9647d9dff1b1cd9a669e30bbbe073
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-21 18:37:39 -0500

    ENG to TLH

--- data/text_boxes.asm
+++ data/text_boxes.asm
@@ -37,15 +37,15 @@ TextBoxTextAndCoordTable:
 	text_box_text JP_POKEDEX_MENU_TEMPLATE,          11,  8, 19, 17, JapanesePokedexMenu,     12, 10
 
 BuySellQuitText:
-	db   "BUY"
-	next "SELL"
-	next "QUIT@"
+	db   "je'"
+	next "ngev"
+	next "mej@"
 
 	db "@" ; unused
 
 UseTossText:
-	db   "USE"
-	next "TOSS@"
+	db   "lo'"
+	next "woD@"
 
 JapaneseSaveMessageText:
 	db   "きろく"
@@ -56,7 +56,7 @@ JapaneseSpeedOptionsText:
 	next "おそい@"
 
 MoneyText:
-	db "MONEY@"
+	db "Huch@"
 
 JapaneseMochimonoText:
 	db "もちもの@"
@@ -66,17 +66,17 @@ JapaneseMainMenuText:
 	next "さいしょから@"
 
 BattleMenuText:
-	db   "FIGHT <PK><MN>"
-	next "ITEM  RUN@"
+	db   "Suv   <PK><MN>"
+	next "luch  HeD@"
 
 SafariZoneBattleMenuText:
-	db   "BALL×       BAIT"
-	next "THROW ROCK  RUN@"
+	db   "moQ×        Soj"
+	next "nagh        HeD@"
 
 SwitchStatsCancelText:
-	db   "SWITCH"
-	next "STATS"
-	next "CANCEL@"
+	db   "qa'"
+	next "Dotlh"
+	next "ratlh@"
 
 JapaneseAhText:
 	db "アッ！@"
