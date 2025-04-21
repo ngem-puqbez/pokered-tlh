0437fb21046cb650c3110ffaba9bec09ff3a20ce
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-20 23:37:23 -0500

    ENG to TLH

--- text/SilphCo4F.asm
+++ text/SilphCo4F.asm
@@ -1,56 +1,54 @@
 _SilphCo4FSilphWorkerMImHidingText::
-	text "Sssh! Can't you"
-	line "see I'm hiding?"
+	text "chuSHa'! jISo'"
+	line "'e' Datlhojbe''a'?"
 	done
 
 _SilphCo4FSilphWorkerMTeamRocketIsGoneText::
-	text "Huh? TEAM ROCKET"
-	line "is gone?"
+	text "nuq? mejpu''a'"
+	line "<p><e><n><g><D><u><j> ghom?"
 	done
 
 _SilphCo4FRocket1BattleText::
-	text "TEAM ROCKET has"
-	line "taken command of"
-	cont "SILPH CO.!"
+	text "<S><I><l><v><a><'> malja'"
+	line "SeHchoH <p><e><n><g><D><u><j>"
+	cont "ghom!"
 	done
 
 _SilphCo4FRocket1EndBattleText::
-	text "Arrgh!"
+	text "QI'yaH!"
 	prompt
 
 _SilphCo4FRocket1AfterBattleText::
-	text "Fwahahaha!"
-	line "My BOSS has been"
-	cont "after this place!"
+	text "vaHghHghHagh!"
+	line "malja'vam Suq 'e'"
+	cont "nIDtaH pInwI'!"
 	done
 
 _SilphCo4FScientistBattleText::
-	text "My #MON are my"
-	line "loyal soldiers!"
+	text "neghwIj matlh"
+	line "bIH #<m><a><n>wIj!"
 	done
 
 _SilphCo4FScientistEndBattleText::
-	text "Darn!"
-	line "You weak #MON!"
+	text "ghuy'!"
+	line "pujbej #<m><a><n>!"
 	prompt
 
 _SilphCo4FScientistAfterBattleText::
-	text "The doors are"
-	line "electronically"
-	cont "locked! A CARD"
-	cont "KEY opens them!"
+	text "ngaQ lojmIt."
+	line "naQHa'moH ya"
+	cont "chaw'."
 	done
 
 _SilphCo4FRocket2BattleText::
-	text "Intruder spotted!"
+	text "yotwI' vISamta'!"
 	done
 
 _SilphCo4FRocket2EndBattleText::
-	text "Who"
-	line "are you?"
+	text "SoH 'Iv?"
 	prompt
 
 _SilphCo4FRocket2AfterBattleText::
-	text "I better tell the"
-	line "BOSS on 11F!"
+	text "pInvaD vIja'meH"
+	line "S11 vIjaHnIS!"
 	done
