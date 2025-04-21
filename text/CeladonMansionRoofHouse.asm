ed238c4284235f30da1df969762b45f8834bcec4
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-21 01:48:47 -0500

    ENG to TLH

--- text/CeladonMansionRoofHouse.asm
+++ text/CeladonMansionRoofHouse.asm
@@ -1,10 +1,9 @@
 _CeladonMansionRoofHouseHikerText::
-	text "I know everything"
-	line "about the world"
-	cont "of #MON in"
-	cont "your GAME BOY!"
+	text "#<m><a><n>mey qo'vam"
+	line "je bopbogh De'"
+	cont "Hoch vISov!"
 
-	para "Get together with"
-	line "your friends and"
-	cont "trade #MON!"
+	para "Sughomchugh SoH"
+	line "juplI' je #<m><a><n>"
+	cont "bomechjaj!"
 	done
