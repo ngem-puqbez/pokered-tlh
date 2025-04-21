07258d96355ee3f441b3a66738177e583729df32
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-20 23:56:11 -0500

    ENG to TLH

--- text/SeafoamIslandsB4F.asm
+++ text/SeafoamIslandsB4F.asm
@@ -1,14 +1,14 @@
 _SeafoamIslandsB4FArticunoBattleText::
-	text "Gyaoo!@"
+	text "ghIya'o!@"
 	text_end
 
 _SeafoamIslandsB4FBouldersSignText::
-	text "Boulders might"
-	line "change the flow"
-	cont "of water!"
+	text "bIQ pep'en lurgh"
+	line "choHlaH naghmey"
+	cont "tIn!"
 	done
 
 _SeafoamIslandsB4FDangerSignText::
-	text "DANGER"
-	line "Fast current!"
+	text "<Q><o><b>! <g><h><u><H>!"
+	line "pep'en HoS!"
 	done
