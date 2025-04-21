ff6ffcfee2b271d689d2eaa4eb61c174026b2a96
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-20 22:16:29 -0500

    ENG to TLH

--- text/VermilionOldRodHouse.asm
+++ text/VermilionOldRodHouse.asm
@@ -1,55 +1,52 @@
 _VermilionOldRodHouseFishingGuruDoYouLikeToFishText::
-	text "I'm the FISHING"
-	line "GURU!"
+	text "ghotI' wamwI'"
+	line "pIn'a' jIH!"
 
-	para "I simply Looove"
-	line "fishing!"
+	para "ghotI'mey vIjon"
+	line "'e' vItIvqu'!"
 
-	para "Do you like to"
-	line "fish?"
+	para "ghotI' Dawam"
+	line "'e' DatIv'a'?"
 	done
 
 _VermilionOldRodHouseFishingGuruTakeThisText::
-	text "Grand! I like"
-	line "your style!"
+	text "maj! butlhlIj"
+	line "vIparHa'!"
 
-	para "Take this and"
-	line "fish, young one!"
+	para "luchvam yItlhap"
+	line "'ej yIwam, puq!"
 
-	para "<PLAYER> received"
-	line "an @"
-	text_ram wStringBuffer
-	text "!@"
+	para "<g><h><o><t><I><'> <r><I><t><w><I><'>"
+	line "Suqpu' <PLAYER>!"
 	text_end
 
 _VermilionOldRodHouseFishingGuruFishingIsAWayOfLifeText::
 	text_start
 
-	para "Fishing is a way"
-	line "of life!"
+	para "lurDech tIQ 'oH"
+	line "chon'e'!"
 
-	para "From the seas to"
-	line "rivers, go out"
-	cont "and land the big"
-	cont "one, young one!"
+	para "bIQtIq ngeng"
+	line "bIQ'a' je yISuch"
+	cont "ghIq gheD nIv"
+	cont "yIjon, QupwI'!"
 	done
 
 _VermilionOldRodHouseFishingGuruThatsSoDisappointingText::
-	text "Oh... That's so"
-	line "disappointing..."
+	text "yaj... "
+	line "chobelHa'moH..."
 	done
 
 _VermilionOldRodHouseFishingGuruHowAreTheFishBitingText::
-	text "Hello there,"
+	text "nuqneH,"
 	line "<PLAYER>!"
 
-	para "How are the fish"
-	line "biting?"
+	para "ngelwI' choptaH'a'"
+	line "ghotI'mey?"
 	done
 
 _VermilionOldRodHouseFishingGuruNoRoomText::
-	text "Oh no!"
+	text "va'!"
 
-	para "You have no room"
-	line "for my gift!"
+	para "nobwIj DalajlaHbe'!"
 	done
