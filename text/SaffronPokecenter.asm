229a65d36c53deb5292cdd34ef54570187781ac4
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-20 23:56:49 -0500

    ENG to TLH

--- text/SaffronPokecenter.asm
+++ text/SaffronPokecenter.asm
@@ -1,12 +1,13 @@
 _SaffronPokecenterBeautyText::
-	text "#MON growth"
-	line "rates differ from"
-	cont "specie to specie."
+	text "jaS HoSchoH"
+	line "#<m><a><n> mut pIm."
+	cont "QIt ghoj 'op 'ej"
+	cont "nom ghoj latlh!"
 	done
 
 _SaffronPokecenterGentlemanText::
-	text "SILPH CO. is very"
-	line "famous. That's"
-	cont "why it attracted"
-	cont "TEAM ROCKET!"
+	text "noyqu' <S><I><l><v><a><'>"
+	line "malja'. meqvammo'"
+	cont "'oH HIv <p><e><n><g><D><u><j>"
+	cont "ghom mIgh!"
 	done
