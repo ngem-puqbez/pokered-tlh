db682119e60aee8de0b92623a30d4fec5e05c1fe
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-21 02:25:22 -0500

    ENG to TLH

--- text/LavenderCuboneHouse.asm
+++ text/LavenderCuboneHouse.asm
@@ -1,25 +1,23 @@
 _LavenderCuboneHouseCuboneText::
-	text "CUBONE: Kyarugoo!@"
+	text "<m><I><v><H><o><m>: qIyarughu!@"
 	text_end
 
 _LavenderCuboneHouseBrunetteGirlPoorCubonesMotherText::
-	text "I hate those"
-	line "horrible ROCKETs!"
+	text "<p><e><n><g><D><u><j> ghom"
+	line "vImuS! mIgh chaH!"
 
-	para "That poor CUBONE's"
-	line "mother..."
+	para "<m><I><v><H><o><m>vam Do'Ha'"
+	line "SoS..."
 
-	para "It was killed"
-	line "trying to escape"
-	cont "from TEAM ROCKET!"
+	para "<p><e><n><g><D><u><j>vo' nargh"
+	line "'e' nIDtaHvIS"
+	cont "'oH HoH chaH!"
 	done
 
 _LavenderCuboneHouseBrunetteGirlGhostIsGoneText::
-	text "The GHOST of"
-	line "#MON TOWER is"
-	cont "gone!"
+	text "ngabpu' molmey"
+	line "chalqach qa'."
 
-	para "Someone must have"
-	line "soothed its"
-	cont "restless soul!"
+	para "ngoj 'oH, 'a"
+	line "jotmoHlaw' vay'!"
 	done
