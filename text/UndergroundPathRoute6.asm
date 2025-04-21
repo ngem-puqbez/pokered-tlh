ec5626f4a1f7c02c7dc69171c96fafc810a88c0a
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-20 22:32:18 -0500

    ENG to TLH

--- text/UndergroundPathRoute6.asm
+++ text/UndergroundPathRoute6.asm
@@ -1,5 +1,5 @@
 _UndergroundPathRoute6GirlText::
-	text "People often lose"
-	line "things in that"
-	cont "UNDERGROUND PATH."
+	text "wutlh tawvamDaq"
+	line "Dochmey law' chIl"
+	cont "ghotpu'."
 	done
