7a73c66ff2dde9fa5b153781648dd0ed2d2708f2
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-21 01:44:13 -0500

    ENG to TLH

--- text/Route5.asm
+++ text/Route5.asm
@@ -1,5 +1,5 @@
 _Route5UndergroundPathSignText::
-	text "UNDERGROUND PATH"
-	line "CERULEAN CITY -"
-	cont "VERMILION CITY"
+	text "<w><u><t><l><h> <t><a><w>"
+	line "<c><h><a><l><H><o><m> veng -"
+	cont "<b><e><q><p><u><j> veng"
 	done
