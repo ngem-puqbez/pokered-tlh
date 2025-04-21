42f7bf3bbd2e4f4887ca0ebaf9614bab012cc302
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-21 18:46:12 -0500

    ENG to TLH

--- data/player_names_list.asm
+++ data/player_names_list.asm
@@ -1,27 +1,27 @@
 IF DEF(_RED)
 DefaultNamesPlayerList:
 	db "NEW NAME@"
-	db "RED@"
-	db "ASH@"
-	db "JACK@"
+	db "<D><o><q>@"
+	db "<q><e><y><l><I><S>@"
+	db "<w><o><'><r><I><v>@"
 
 DefaultNamesRivalList:
 	db "NEW NAME@"
-	db "BLUE@"
-	db "GARY@"
-	db "JOHN@"
+	db "<S><u><D>@"
+	db "<m><o><l><o><r>@"
+	db "<D><u><r><a><S>@"
 ENDC
 
 IF DEF(_BLUE)
 DefaultNamesPlayerList:
 	db "NEW NAME@"
-	db "BLUE@"
-	db "GARY@"
-	db "JOHN@"
+	db "<S><u><D>@"
+	db "<m><o><l><o><r>@"
+	db "<D><u><r><a><S>@"
 
 DefaultNamesRivalList:
 	db "NEW NAME@"
-	db "RED@"
-	db "ASH@"
-	db "JACK@"
+	db "<D><o><q>@"
+	db "<q><e><y><l><I><S>@"
+	db "<w><o><'><r><I><v>@"
 ENDC
