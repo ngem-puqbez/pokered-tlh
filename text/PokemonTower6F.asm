ce6262883a588e518d8c79cd822c5c1a31f83507
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-22 03:59:52 -0500

    ENG to TLH

--- text/PokemonTower6F.asm
+++ text/PokemonTower6F.asm
@@ -1,61 +1,58 @@
 _PokemonTower6FGhostWasCubonesMotherText::
-	text "The GHOST was the"
-	line "restless soul of"
-	cont "CUBONE's mother!"
+	text "<m><I><v><H><o><m> SoS qa'"
+	line "'oH qa' ghan'e'."
 	done
 
 _PokemonTower6FSoulWasCalmedText::
-	text "The mother's soul"
-	line "was calmed."
+	text "ghanHa'choH"
+	line "SoS qa'."
 
-	para "It departed to"
-	line "the afterlife!"
+	para "qo'vam tlheDta'!"
 	done
 
 _PokemonTower6FChanneler1BattleText::
-	text "Give...me..."
-	line "blood..."
+	text "'Iw..."
+	line "HI...nob..."
 	done
 
 _PokemonTower6FChanneler1EndBattleText::
-	text "Groan!"
+	text "qIn!"
 	prompt
 
 _PokemonTower6FChanneler1AfterBattleText::
-	text "I feel anemic and"
-	line "weak..."
+	text "jIpujlaw'..."
+	line "bIQ rur 'IwwIj..."
 	done
 
 _PokemonTower6FChanneler2BattleText::
-	text "Urff... Kwaah!"
+	text "'ughghgh... quwa'!"
 	done
 
 _PokemonTower6FChanneler2EndBattleText::
-	text "Some-"
-	line "thing fell out!"
+	text "toH,"
+	line "nachwIjvo' pum!"
 	prompt
 
 _PokemonTower6FChanneler2AfterBattleText::
-	text "Hair didn't fall"
-	line "out! It was an"
-	cont "evil spirit!"
+	text "pumbe' jIbwIj!"
+	line "qa' mIgh 'oH!"
 	done
 
 _PokemonTower6FChanneler3BattleText::
-	text "Ke..ke...ke..."
-	line "ke..ke...ke!!"
+	text "qI...qu...qa..."
+	line "qu...qe...qo!!"
 	done
 
 _PokemonTower6FChanneler3EndBattleText::
-	text "Keee!"
+	text " "
+	line "qIyIyIy!"
 	prompt
 
 _PokemonTower6FChanneler3AfterBattleText::
-	text "What's going on"
-	line "here?"
+	text "naDev qaSlI' nuq?"
 	done
 
 _PokemonTower6FBeGoneText::
-	text "Be gone..."
-	line "Intruders..."
+	text "pemej..."
+	line "yotwI'pu'..."
 	done
