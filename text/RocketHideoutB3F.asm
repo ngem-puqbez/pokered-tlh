53bfde0e839718c1ead45e76fc64862d62530e95
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-22 04:17:30 -0500

    ENG to TLH

--- text/RocketHideoutB3F.asm
+++ text/RocketHideoutB3F.asm
@@ -1,35 +1,34 @@
 _RocketHideoutB3FRocket1BattleText::
-	text "Stop meddling in"
-	line "TEAM ROCKET's"
-	cont "affairs!"
+	text "<p><e><n><g><D><u><j> ghom"
+	line "Soj DanIS 'e'"
+	cont "yImev!"
 	done
 
 _RocketHideoutB3FRocket1EndBattleText::
-	text "Oof!"
-	line "Taken down!"
+	text "Qo'!"
+	line "jItuH!"
 	prompt
 
 _RocketHideoutB3FRocket1AfterBattleText::
-	text "SILPH SCOPE?"
-	line "The machine the"
-	cont "BOSS stole. It's"
-	cont "here somewhere."
+	text "<S><o><'><H><a><'><m><o><H><w><I><'>?"
+	line "janvetlh nIH pIn."
+	cont "waw'vamDaq 'oH."
 	done
 
 _RocketHideout3BattleText::
-	text "We got word from"
-	line "upstairs that you"
-	cont "were coming!"
+	text "maHvaD bIyottaH"
+	line "'e' ghuHmoHta'"
+	cont "Su'roq jen beq!"
 	done
 
 _RocketHideout3EndBattleText3::
-	text "What?"
-	line "I lost? No!"
+	text "chay'?"
+	line "jIlujlaH'a'? Qo'!"
 	prompt
 
 _RocketHide3AfterBattleText3::
-	text "Go ahead and go!"
-	line "But, you need the"
-	cont "LIFT KEY to run"
-	cont "the elevator!"
+	text "HImej! yIruch!"
+	line "'ach tut Duj"
+	cont "Dalo'meH <t><u><t>"
+	cont "<D><u><j> <c><h><a><w><'> DapoQ!"
 	done
