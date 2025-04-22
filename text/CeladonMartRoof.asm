b7a66f98aed59165982e0a43a4fd11459c6f3916
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-21 22:27:39 -0500

    ENG to TLH

--- text/CeladonMartRoof.asm
+++ text/CeladonMartRoof.asm
@@ -1,152 +1,145 @@
 _CeladonMartRoofLittleGirlGiveHerWhichDrinkText::
-	text "Give her which"
-	line "drink?"
+	text "Soj Danobbogh"
+	line "yIwIv."
 	done
 
 _CeladonMartRoofLittleGirlYayFreshWaterText::
-	text "Yay!"
+	text "maj!"
 
-	para "FRESH WATER!"
+	para "<b><I><Q> <n><I><t>!"
 
-	para "Thank you!"
+	para "jI'oj!"
 
-	para "You can have this"
-	line "from me!@"
+	para "'ej SoHvaD luchvam"
+	line "vInob!@"
 	text_end
 
 _CeladonMartRoofLittleGirlReceivedTM13Text::
-	text "<PLAYER> received"
-	line "@"
 	text_ram wStringBuffer
-	text "!@"
+	text " Suq"
+	cont "<PLAYER>."
 	text_end
 
 _CeladonMartRoofLittleGirlTM13ExplanationText::
 	text_start
 
-	para "@"
+	para "<c><h><u><c><h> <t><I><H> tonSaw'"
+	line "ngaS @"
 	text_ram wStringBuffer
-	text " contains"
-	line "ICE BEAM!"
+	text "!"
 
-	para "It can freeze the"
-	line "target sometimes!@"
+	para "rut DoSDaj taDmoH!@"
 	text_end
 
 _CeladonMartRoofLittleGirlYaySodaPopText::
-	text "Yay!"
+	text "maj!"
 
-	para "SODA POP!"
+	para "<'><a><w><j><e><'>!"
 
-	para "Thank you!"
+	para "jI'oj!"
 
-	para "You can have this"
-	line "from me!@"
+	para "'ej SoHvaD luchvam"
+	line "vInob!@"
 	text_end
 
 _CeladonMartRoofLittleGirlReceivedTM48Text::
-	text "<PLAYER> received"
-	line "@"
 	text_ram wStringBuffer
-	text "!@"
+	text " Suq"
+	cont "<PLAYER>."
 	text_end
 
 _CeladonMartRoofLittleGirlTM48ExplanationText::
 	text_start
 
-	para "@"
+	para "<p><u><m> <n><a><g><h> tonSaw'"
+	line "ngaS @"
 	text_ram wStringBuffer
-	text " contains"
-	line "ROCK SLIDE!"
+	text "!"
 
-	para "It can spook the"
-	line "target sometimes!@"
+	para "rut DoSDaj murmoH!@"
 	text_end
 
 _CeladonMartRoofLittleGirlYayLemonadeText::
-	text "Yay!"
+	text "maj!"
 
-	para "LEMONADE!"
+	para "<v><I><y><c><h><o><r><g><h>!"
 
-	para "Thank you!"
+	para "jI'oj!"
 
-	para "You can have this"
-	line "from me!@"
+	para "'ej SoHvaD luchvam"
+	line "vInob!@"
 	text_end
 
 _CeladonMartRoofLittleGirlReceivedTM49Text::
-	text "<PLAYER> received"
-	line "TM49!@"
+	text "<t><j>49 Suq"
+	line "<PLAYER>!@"
 	text_end
 
 _CeladonMartRoofLittleGirlTM49ExplanationText::
 	text_start
 
-	para "TM49 contains"
-	line "TRI ATTACK!@"
+	para "<w><e><j><l><o><g><h> <H><I><v>"
+	line "tonSaw' ngaS <t><j>49!@"
 	text_end
 
 _CeladonMartRoofLittleGirlNoRoomText::
-	text "You don't have"
-	line "space for this!@"
+	text "buy'mo' buqlIj"
+	line "DatlhaplaHbe'!@"
 	text_end
 
 _CeladonMartRoofLittleGirlImNotThirstyText::
-	text "No thank you!"
-	line "I'm not thirsty"
-	cont "after all!@"
+	text "jIwIvqa'!"
+	line "DaH jI'ojqu'be'!@"
 	text_end
 
 _CeladonMartRoofSuperNerdText::
-	text "My sister is a"
-	line "trainer, believe"
-	cont "it or not."
+	text "vabDot chojwI' ghaH"
+	line "be'nI'wI' Qup'e'."
 
-	para "But, she's so"
-	line "immature, she"
-	cont "drives me nuts!"
+	para "'ach Doghqu'mo'"
+	line "munuQlaHqu'!"
 	done
 
 _CeladonMartRoofLittleGirlImThirstyText::
-	text "I'm thirsty!"
-	line "I want something"
-	cont "to drink!"
+	text "jI'oj!"
+	line "vay' vItlhutlh"
+	cont "nIneHqu'!"
 	done
 
 _CeladonMartRoofLittleGirlGiveHerADrinkText::
-	text "I'm thirsty!"
-	line "I want something"
-	cont "to drink!"
+	text "jI'oj!"
+	line "vay' vItlhutlh"
+	cont "nIneHqu'!"
 
-	para "Give her a drink?"
+	para "ghaHvaD Soj"
+	line "Danob DaneH'a'?"
 	done
 
 _CeladonMartRoofCurrentFloorSignText::
-	text "ROOFTOP SQUARE:"
-	line "VENDING MACHINES"
+	text "beb yotlh:"
+	line "ngevwI' janmey"
 	done
 
 _VendingMachineText1::
-	text "A vending machine!"
-	line "Here's the menu!"
+	text "ngevwI' jan 'oH!"
+	line "HIDjolev tu'lu'!"
 	prompt
 
 _VendingMachineText4::
-	text "Oops, not enough"
-	line "money!"
+	text "Hu... yapbe'"
+	line "HuchlIj!"
 	done
 
 _VendingMachineText5::
 	text_ram wStringBuffer
 	text_start
-	line "popped out!"
+	line "peS jan!"
 	done
 
 _VendingMachineText6::
-	text "There's no more"
-	line "room for stuff!"
+	text "wejHa' buy' buq!"
 	done
 
 _VendingMachineText7::
-	text "Not thirsty!"
+	text "bI'ojbe'bej!"
 	done
