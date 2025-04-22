ba6bc064812a2dcb9e2ad3960839c8770174ecb7
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-22 03:47:14 -0500

    ENG to TLH

--- text/PokemonTower1F.asm
+++ text/PokemonTower1F.asm
@@ -1,31 +1,31 @@
 _PokemonTower1FReceptionistText::
-	text "#MON TOWER was"
-	line "erected in the"
-	cont "memory of #MON"
-	cont "that had died."
+	text "#<m><a><n>mey"
+	line "Heghpu'bogh"
+	cont "vanmeH <m><o><l><m><e><y>"
+	cont "<c><h><a><l><q><a><c><h> mutlhlu'."
 	done
 
 _PokemonTower1FMiddleAgedWomanText::
-	text "Did you come to"
-	line "pay respects?"
-	cont "Bless you!"
+	text "HeghwI' vanmeH"
+	line "DaSuchtaH'a'?"
+	cont "qaquvmoH!"
 	done
 
 _PokemonTower1FBaldingGuyText::
-	text "I came to pray"
-	line "for my CLEFAIRY."
+	text "<j><a><g><h><'><I><v> vIqawmeH"
+	line "molDaj vISuch."
 
-	para "Sniff! I can't"
-	line "stop crying..."
+	para "Qoghogh! jISaQtaH"
+	line "'e' vImevlaHbe'..."
 	done
 
 _PokemonTower1FGirlText::
-	text "My GROWLITHE..."
-	line "Why did you die?"
+	text "<q><u><l><v><I><j>wIj..."
+	line "qatlh bIHeghnIS?"
 	done
 
 _PokemonTower1FChannelerText::
-	text "I am a CHANNELER!"
-	line "There are spirits"
-	cont "up to mischief!"
+	text "qa' jatlhwI' jIH!"
+	line "naDev SengtaH"
+	cont "qa'mey QeH!"
 	done
