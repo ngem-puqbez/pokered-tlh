410300826654de9dfa11c027358205e37c638c38
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-22 01:53:20 -0500

    ENG to TLH

--- text/Route17.asm
+++ text/Route17.asm
@@ -1,195 +1,195 @@
 _Route17Biker1BattleText::
-	text "There's no money"
-	line "in fighting kids!"
+	text "puq neH vISuvchugh"
+	line "Huch vISuqbe'!"
 	done
 
 _Route17Biker1EndBattleText::
-	text "Burned"
-	line "out!"
+	text "natlh!"
 	prompt
 
 _Route17Biker1AfterBattleText::
-	text "Good stuff is"
-	line "lying around on"
-	cont "CYCLING ROAD!"
+	text "<q><a><m> <D><o> <D><u><j>Daq"
+	line "law' luch"
+	cont "lonpu'lubogh!"
 	done
 
 _Route17Biker2BattleText::
-	text "What do you want,"
-	line "kiddo?"
+	text "nuqneH,"
+	line "puqoy?"
 	done
 
 _Route17Biker2EndBattleText::
-	text "Whoo!"
+	text "maj!"
 	prompt
 
 _Route17Biker2AfterBattleText::
-	text "I could belly-"
-	line "bump you outta"
-	cont "here!"
+	text "naDevvo' qavo'laH!"
+	line "chorwIj qapaw'moH"
+	cont "'e' vInIDchugh!"
 	done
 
 _Route17Biker3BattleText::
-	text "You heading to"
-	line "FUCHSIA?"
+	text "<t><a><r><D><e><H> <v><e><n><g>Daq"
+	line "DajaHlI'a'?"
 	done
 
 _Route17Biker3EndBattleText::
-	text "Crash and"
-	line "burn!"
+	text "jIpum!"
+	line "yav vIngeQqu'!"
 	prompt
 
 _Route17Biker3AfterBattleText::
-	text "I love racing"
-	line "downhill!"
+	text "bo'ghey vIghIrtaH"
+	line "'e' vItIvqu'!"
 	done
 
 _Route17Biker4BattleText::
-	text "We're BIKERs!"
-	line "Highway stars!"
+	text "matlhay'Ha'!"
+	line "jonta' yIlaQ!"
 	done
 
 _Route17Biker4EndBattleText::
-	text "Smoked!"
+	text "yInlIj"
+	cont "DayInqu'ba'!"
 	prompt
 
 _Route17Biker4AfterBattleText::
-	text "Are you looking"
-	line "for adventure?"
+	text "may'mey bInej'a'?"
 	done
 
 _Route17Biker5BattleText::
-	text "Let VOLTORB"
-	line "electrify you!"
+	text "Dumupbej <r><I><H><m><o><Q>wIj"
+	line "'ul; pe'bIl rur!"
 	done
 
 _Route17Biker5EndBattleText::
-	text "Grounded"
-	line "out!"
+	text "yavDaq"
+	line "jIHot!"
 	prompt
 
 _Route17Biker5AfterBattleText::
-	text "I got my VOLTORB"
-	line "at the abandoned"
-	cont "POWER PLANT."
+	text "ngo'bogh HoS waw'"
+	line "chImDaq <r><I><H><m><o><Q>"
+	cont "vIjon."
 	done
 
 _Route17Biker6BattleText::
-	text "My #MON won't"
-	line "evolve! Why?"
+	text "jImIS. ghe'be'"
+	line "#<m><a><n>wIj! qatlh?"
 	done
 
 _Route17Biker6EndBattleText::
-	text "Why,"
-	line "you!"
+	text "toH!"
+	line "SoH!"
 	prompt
 
 _Route17Biker6AfterBattleText::
-	text "Maybe some #MON"
-	line "need element"
-	cont "STONEs to evolve."
+	text "Hu... ghe'meH"
+	line "naghmey lI'"
+	cont "poQ #<m><a><n> puS 'e'"
+	cont "vIQoypu..."
 	done
 
 _Route17Biker7BattleText::
-	text "I need a little"
-	line "exercise!"
+	text "DaH jIqeqtaH!"
+	line "chotlhej'a'?"
 	done
 
 _Route17Biker7EndBattleText::
-	text "Whew!"
-	line "Good workout!"
+	text "maj!"
+	line "maqeqchu'!"
 	prompt
 
 _Route17Biker7AfterBattleText::
-	text "I'm sure I lost"
-	line "weight there!"
+	text "tugh tlhaghwIj"
+	line "vIweSbej!"
 	done
 
 _Route17Biker8BattleText::
-	text "Be a rebel!"
+	text "yIlotlh!"
 	done
 
 _Route17Biker8EndBattleText::
-	text "Aaaargh!"
+	text "Qu'vaaa!"
 	prompt
 
 _Route17Biker8AfterBattleText::
-	text "Be ready to fight"
-	line "for your beliefs!"
+	text "ghoblIj DaHubmeH"
+	line "bIghobqang'a'?"
 	done
 
 _Route17Biker9BattleText::
-	text "Nice BIKE!"
-	line "How's it handle?"
+	text "waghlaw' <q><a><m> <D><o>"
+	line "<D><u><j>lIj! QaQ'a'?"
 	done
 
 _Route17Biker9EndBattleText::
-	text "Shoot!"
+	text "va!"
 	prompt
 
 _Route17Biker9AfterBattleText::
-	text "The slope makes"
-	line "it hard to steer!"
+	text "DujwIj vIraQtaH"
+	line "'ach QatlhmoH"
+	cont "bo'gheyvam!"
 	done
 
 _Route17Biker10BattleText::
-	text "Get lost kid!"
-	line "I'm bushed!"
+	text "HImej, puq!"
+	line "jIDoy'!"
 	done
 
 _Route17Biker10EndBattleText::
-	text "Are you"
-	line "satisfied?"
+	text "vaj?"
+	line "bIyaj'a'?"
 	prompt
 
 _Route17Biker10AfterBattleText::
-	text "I need to catch"
-	line "a few Zs!"
+	text "jIDumnIS."
+	line "ngaj len!"
 	done
 
 _Route17NoticeSign1Text::
-	text "It's a notice!"
+	text "vep 'oH!"
 
-	para "Watch out for"
-	line "discarded items!"
+	para "luch woDlu'pu'bogh"
+	line "yIghuH!"
 	done
 
 _Route17TrainerTips1Text::
-	text "TRAINER TIPS"
+	text "chojwI' qeS"
 
-	para "All #MON are"
-	line "unique."
+	para "pIm Hoch #<m><a><n>."
 
-	para "Even #MON of"
-	line "the same type and"
-	cont "level grow at"
-	cont "different rates."
+	para "vabDot rapchugh"
+	line "mutchaj patlhchaj"
+	cont "je pIm HoSmey"
+	cont "ghaj."
 	done
 
 _Route17TrainerTips2Text::
-	text "TRAINER TIPS"
+	text "chojwI' qeS"
 
-	para "Press the A or B"
-	line "Button to stay in"
-	cont "place while on a"
-	cont "slope."
+	para "vIHHa''eghmoHmeH"
+	line "[a] leQ [b] leQ"
+	cont "pagh Da'uylaH."
 	done
 
 _Route17SignText::
-	text "ROUTE 17"
-	line "CELADON CITY -"
-	cont "FUCHSIA CITY"
+	text "He wa'maH Soch"
+	line "<n><g><o><p><w><a><g><h> <v><e><n><g> -"
+	cont "<t><a><r><D><e><H> <v><e><n><g>"
 	done
 
 _Route17NoticeSign2Text::
-	text "It's a notice!"
+	text "vep 'oH!"
 
-	para "Don't throw the"
-	line "game, throw #"
-	cont "BALLs instead!"
+	para "Quj yIvo'Qo'!"
+	line "#<m><o><Q> neH"
+	cont "yIvo'!"
 	done
 
 _Route17CyclingRoadEndsSignText::
-	text "CYCLING ROAD"
-	line "Slope ends here!"
+	text "<q><a><m> <D><o> <D><u><j> <t><a><w>"
+	line "naDev 'oH bo'ghey"
+	cont "qa'rI''e'."
 	done
