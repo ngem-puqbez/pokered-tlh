78a5fa034997d1342047a6d6526e8a2809d225c1
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-21 20:36:56 -0500

    ENG to TLH

--- text/Route3.asm
+++ text/Route3.asm
@@ -1,145 +1,144 @@
 _Route3Text1::
-	text "Whew... I better"
-	line "take a rest..."
-	cont "Groan..."
+	text "Hu'ma... DaH"
+	line "jIleSnISqu'..."
+	cont "ghghgh..."
 
-	para "That tunnel from"
-	line "CERULEAN takes a"
-	cont "lot out of you!"
+	para "<c><h><a><l><H><o><m> <v><e><n><g>vo'"
+	line "jIyItpu' 'ej "
+	cont "DaH jIDoy'!"
 	done
 
 _Route3Youngster1BattleText::
-	text "Hey! I met you in"
-	line "VIRIDIAN FOREST!"
+	text "toH! <q><o><'><r><I><n>"
+	line "<n><g><e><m>Daq"
+	cont "maqIHchuqpu'!"
 	done
 
 _Route3Youngster1EndBattleText::
-	text "You"
-	line "beat me again!"
+	text "ch..."
+	line "chojeyqa'!"
 	prompt
 
 _Route3Youngster1AfterBattleText::
-	text "There are other"
-	line "kinds of #MON"
-	cont "than those found"
-	cont "in the forest!"
+	text "ngem #<m><a><n>"
+	line "bIHbe' Hoch "
+	cont "#<m><a><n>'e'!"
 	done
 
 _Route3Youngster2BattleText::
-	text "Hi! I like shorts!"
-	line "They're comfy and"
-	cont "easy to wear!"
+	text "yopwaH bID vImaS."
+	line "muchurHa'qu'moH!"
 	done
 
 _Route3Youngster2EndBattleText::
-	text "I don't"
-	line "believe it!"
+	text " "
+	line "vIHarqangbe'!"
 	prompt
 
 _Route3Youngster2AfterBattleText::
-	text "Are you storing"
-	line "your #MON on"
-	cont "PC? Each BOX can"
-	cont "hold 20 #MON!"
+	text "De'wI'Daq"
+	line "#<m><a><n>lIj"
+	cont "Dapol'a'?"
+	cont "20 #<m><a><n>"
+	cont "ngaSlaH ngIq ta."
 	done
 
 _Route3CooltrainerF1BattleText::
-	text "You looked at me,"
-	line "didn't you?"
+	text "choghanpu'"
+	line "qar'a'?"
 	done
 
 _Route3CooltrainerF1EndBattleText::
-	text "You're"
-	line "mean!"
+	text "bIDoch!"
 	prompt
 
 _Route3CooltrainerF1AfterBattleText::
-	text "Quit staring if"
-	line "you don't want to"
-	cont "fight!"
+	text "maSuv"
+	line "DaneHbe'chugh vaj"
+	cont "bIghantaHnISbe'!"
 	done
 
 _Route3Youngster3BattleText::
-	text "Are you a trainer?"
-	line "Let's fight!"
+	text "chojwI SoH'a'?"
+	line "HISuv!"
 	done
 
 _Route3Youngster3EndBattleText::
-	text "If I"
-	line "had new #MON I"
-	cont "would've won!"
+	text "ghew"
+	line "chu' vIghajchugh"
+	cont "jIQaplaH!"
 	prompt
 
 _Route3Youngster3AfterBattleText::
-	text "If a #MON BOX"
-	line "on the PC gets"
-	cont "full, just switch"
-	cont "to another BOX!"
+	text "De'wI'Daq"
+	line "buy'choHchugh ta"
+	cont "vaj ta chu' yIwIv"
+	cont "neH 'e' DapoQ!"
 	done
 
 _Route3CooltrainerF2BattleText::
-	text "That look you"
-	line "gave me, it's so"
-	cont "intriguing!"
+	text "jaS chochotlh."
+	line "DaH chovuQ."
 	done
 
 _Route3CooltrainerF2EndBattleText::
-	text "Be nice!"
+	text "yIDochHa'!"
 	prompt
 
 _Route3CooltrainerF2AfterBattleText::
-	text "Avoid fights by"
-	line "not letting"
-	cont "people see you!"
+	text "maSuvchoH"
+	line "DaneHbe'chugh"
+	cont "HIchotlhQo'!"
 	done
 
 _Route3Youngster4BattleText::
-	text "Hey! You're not"
-	line "wearing shorts!"
+	text "toH! yopwaH bID"
+	line "DatuQbe'!"
 	done
 
 _Route3Youngster4EndBattleText::
-	text "Lost!"
-	line "Lost! Lost!"
+	text "jIlujqu'!"
+	line "jIluj! luj!"
 	prompt
 
 _Route3Youngster4AfterBattleText::
-	text "I always wear"
-	line "shorts, even in"
-	cont "winter!"
+	text "reH yopwaH bID"
+	line "vItuQ. vabDot"
+	cont "qImroq bIr"
+	cont "qaStaHvIS!"
 	done
 
 _Route3Youngster5BattleText::
-	text "You can fight my"
-	line "new #MON!"
+	text "#<m><a><n>wIj chu'"
+	line "DaSuv vIneH!"
 	done
 
 _Route3Youngster5EndBattleText::
-	text "Done"
-	line "like dinner!"
+	text "rIn;"
+	line "'uQ rur!"
 	prompt
 
 _Route3Youngster5AfterBattleText::
-	text "Trained #MON"
-	line "are stronger than"
-	cont "the wild ones!"
+	text "#<m><a><n> tlhay'"
+	line "HoS law' #<m><a><n>"
+	cont "tlhab HoS puS!"
 	done
 
 _Route3CooltrainerF3BattleText::
-	text "Eek! Did you"
-	line "touch me?"
+	text "Qo'!"
+	line "choHot'a'?"
 	done
 
 _Route3CooltrainerF3EndBattleText::
-	text "That's it?"
+	text "rIn'a'?"
 	prompt
 
 _Route3CooltrainerF3AfterBattleText::
-	text "ROUTE 4 is at the"
-	line "foot of MT.MOON."
+	text "<m><a><S> <H><u><D> bIngDaq"
+	line "'oH He loS'e'."
 	done
 
 _Route3SignText::
-	text "ROUTE 3"
-	line "MT.MOON AHEAD"
+	text "He wej"
+	line "Sum <m><a><S> <H><u><D>"
 	done
