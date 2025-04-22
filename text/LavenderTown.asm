dfc2d0e67472224f33ff79209bd9f1ee9f638f1a
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-22 03:03:32 -0500

    ENG to TLH

--- text/LavenderTown.asm
+++ text/LavenderTown.asm
@@ -1,64 +1,64 @@
 _LavenderTownLittleGirlDoYouBelieveInGhostsText::
-	text "Do you believe in"
-	line "GHOSTs?"
+	text "yInbogh qa'mey"
+	line "DaHar'a'?"
 	done
 
 _LavenderTownLittleGirlSoThereAreBelieversText::
-	text "Really? So there"
-	line "are believers..."
+	text "teH'a'? vaj"
+	line "HarwI' lutu'lu'."
 	done
 
 _LavenderTownLittleGirlHaHaGuessNotText::
-	text "Hahaha, I guess"
-	line "not."
+	text "HghHgh... bIlugh"
+	line "'e' vI'Ir."
 
-	para "That white hand"
-	line "on your shoulder,"
-	cont "it's not real."
+	para "volchaHlIjDaq"
+	line "leSbogh ghop Dem"
+	cont "vIleghbe'bej."
 	done
 
 _LavenderTownCooltrainerMText::
-	text "This town is known"
-	line "as the grave site"
-	cont "of #MON."
+	text "#<m><a><n> mol Daq"
+	line "'oH vengHomvam."
+	cont "'ISyarmaj 'oH."
 
-	para "Memorial services"
-	line "are held in"
-	cont "#MON TOWER."
+	para "Hoch jaj <m><o><l><m><e><y>"
+	line "<c><h><a><l><q><a><c><h>Daq nol"
+	cont "qaS."
 	done
 
 _LavenderTownSuperNerdText::
-	text "GHOSTs appeared"
-	line "in #MON TOWER."
+	text "chalqachDaq"
+	line "nargh qa' QeH."
 
-	para "I think they're"
-	line "the spirits of"
-	cont "#MON that the"
-	cont "ROCKETs killed."
+	para "#<m><a><n>'e'"
+	line "HoHpu'bogh"
+	cont "<p><e><n><g><D><u><j> ghom"
+	cont "bIH 'e' vIHar."
 	done
 
 _LavenderTownSignText::
-	text "LAVENDER TOWN"
-	line "The Noble Purple"
-	cont "Town"
+	text "'<u><m><a><'> <v><e><n><g><H><o><m>"
+	line "SuD vengHomvam"
+	cont "quv 'ej Doq."
 	done
 
 _LavenderTownSilphScopeSignText::
-	text "New SILPH SCOPE!"
+	text "<S><o><'><H><a><'><m><o><H><w><I><'> chu'!"
 
-	para "Make the Invisible"
-	line "Plain to See!"
+	para "So'bogh Doch"
+	line "yI'ang 'ej yIlegh!"
 
-	para "SILPH CO."
+	para "<S><I><l><v><a><'> malja'"
 	done
 
 _LavenderTownPokemonHouseSignText::
-	text "LAVENDER VOLUNTEER"
-	line "#MON HOUSE"
+	text "<'><u><m><a><'> <v><e><n><g><H><o><m>"
+	line "#<m><a><n> QorghwI'"
 	done
 
 _LavenderTownPokemonTowerSignText::
-	text "May the Souls of"
-	line "#MON Rest Easy"
-	cont "#MON TOWER"
+	text "pe'vIlHa' leSjaj"
+	line "#<m><a><n> Hoch"
+	cont "<m><o><l><m><e><y> <c><h><a><l><q><a><c><h>"
 	done
