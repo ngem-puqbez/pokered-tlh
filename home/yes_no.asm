c063c5bd48b80c15d69885b70874c5d7ea853b90
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-25 01:45:42 -0500

    widened box for HIja'/ghobe'

--- home/yes_no.asm
+++ home/yes_no.asm
@@ -14,8 +14,8 @@ TwoOptionMenu:: ; unreferenced
 InitYesNoTextBoxParameters::
 	xor a ; YES_NO_MENU
 	ld [wTwoOptionMenuID], a
-	hlcoord 14, 7
-	lb bc, 8, 15
+	hlcoord 12, 7
+	lb bc, 8, 13
 	ret
 
 YesNoChoicePokeCenter::
