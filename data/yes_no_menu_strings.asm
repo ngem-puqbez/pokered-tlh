012ac8c8915ab9dd1a93d9a5e45b6870d933e7ae
ngem-puqbez <LawlietMyka@gmail.com>
2025-04-25 04:02:46 -0500

    Update yes_no_menu_strings.asm

--- data/yes_no_menu_strings.asm
+++ data/yes_no_menu_strings.asm
@@ -7,14 +7,14 @@ TwoOptionMenuStrings:
 ; entries correspond to *_MENU constants
 	table_width 5
 	; width, height, blank line before first menu item?, text pointer
-	two_option_menu 4, 3, FALSE, .YesNoMenu
+	two_option_menu 6, 3, FALSE, .YesNoMenu
 	two_option_menu 6, 3, FALSE, .NorthWestMenu
 	two_option_menu 6, 3, FALSE, .SouthEastMenu
 	two_option_menu 6, 3, FALSE, .YesNoMenu
 	two_option_menu 6, 3, FALSE, .NorthEastMenu
-	two_option_menu 7, 3, FALSE, .TradeCancelMenu
+	two_option_menu 4, 3, FALSE, .TradeCancelMenu
 	two_option_menu 7, 4, TRUE,  .HealCancelMenu
-	two_option_menu 4, 3, FALSE, .NoYesMenu
+	two_option_menu 6, 3, FALSE, .NoYesMenu
 	assert_table_length NUM_TWO_OPTION_MENUS
 
 .NoYesMenu:
